6ed6904851665e0764fea7b2c7e329295fceee3f
Randall Bohn (dexter) <rsbohn@gmail.com>
2024-02-10 10:54:24 -0700

    printing activator

--- main.c
+++ main.c
@@ -5,14 +5,61 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "nsio.h"
+#include "sio.h"
 
 const char *api = "N:HTTPS://api.pota.app/spot/activator";
 
-char query[256];
 char buffer[256];
-void main(void) {
+
+unsigned char query(char *path) {
     unsigned char err;
     int n;
-    putchar(CH_CLR);
-    
+    err = njsonquery(1, SIO_RW, path);
+    buffer[0] = '\0';
+    n = (OS.dvstat[1]<<8)+OS.dvstat[0];
+    err = nread(1, buffer, n);
+    if (err == 1) {
+        puts(buffer);
+    } else {
+        puts("ERR");
+    }
+    return err;
+}
+void main(void) {
+    unsigned char err;
+    unsigned char c;
+    int n;
+
+    /* putchar(CH_CLR); */
+    puts("POTA Parks On The Air");
+    err = nopen(1, (char *) api, SIO_RW);
+    err = nchanmode(1, SIO_RW, CHANNELMODE_JSON);
+    err = njsonparse(1, SIO_RW);
+
+    err = njsonquery(1, SIO_RW, "/0/activator");
+    if (err == 1) {
+        n = (OS.dvstat[1] << 8) + OS.dvstat[0];
+        if (n == 0) {
+            buffer[0] = '\0';
+            err = nread(1, buffer, 32);
+            if (err == 1) {
+                puts(buffer);
+            } else {
+                puts("ERR: nread");
+            }
+        } else {
+            puts("ERR: zero");
+        }
+    } else {
+        puts("ERR: njsonquery");
+    }
+    puts("DONE!");
+    OS.ch = KEY_NONE;
+    do {
+        c = OS.ch;
+    } while (c == KEY_NONE);
+
+    if (c == KEY_ESC) {
+        abort();
+    }
 }
