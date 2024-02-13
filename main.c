f58b75c6cc420c6688d7807762dbfc5d90652f30
Randall Bohn (dexter) <rsbohn@gmail.com>
2024-02-13 05:16:58 -0700

    slowprint

--- main.c
+++ main.c
@@ -4,24 +4,46 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 #include <unistd.h>
 #include "nsio.h"
 #include "sio.h"
 
+#define SCR_WIDTH (40)
 const char *api = "N:HTTPS://api.pota.app/spot/activator";
-
 char buffer[256];
 
+void slowly() {
+    clock_t now = clock();
+    int x = 0;
+    while (clock() < now + 10)
+        /* burn some cycles*/
+        x = (x + 1);
+}
+
+void text(char *s) {
+    int x;
+    char ch;
+    for (x=0; x < SCR_WIDTH; x++) {
+        ch = s[x];
+        if (ch == 0x9B) break;
+        putchar(ch);
+        slowly();
+    }
+    putchar(' ');
+}
 unsigned char query(char *path) {
     unsigned char err;
     int n;
+
     err = njsonquery(1, SIO_RW, path);
     err = nstatus(1);
     buffer[0] = '\0';
     n = (OS.dvstat[1]<<8)+OS.dvstat[0];
-    err = nread(1, buffer, 32);
+    err = nread(1, buffer, n);
+
     if (err == 1) {
-        puts(buffer);
+        text(buffer);
     } else {
         puts("ERR");
     }
@@ -30,7 +52,7 @@ unsigned char query(char *path) {
 int main(void) {
     unsigned char err;
 
-    /* putchar(CH_CLR); */
+    putchar(CH_CLR);
     puts("POTA Parks On The Air");
     err = nopen(1, (char *) api, SIO_RW);
     err = nchanmode(1, SIO_RW, CHANNELMODE_JSON);
@@ -38,12 +60,18 @@ int main(void) {
 
     /* TODO: Format the display. */    
     query("/0/spotId");
-    query("/0/spotTime");
     query("/0/activator");
+    query("/0/spotTime");
+    putchar('\n');
+    query("/1/spotId");
+    query("/1/activator");
+    query("/1/spotTime");
+    putchar('\n');
 
     err = nchanmode(1, SIO_RW, CHANNELMODE_PROTOCOL);
     err = nclose(1);
 
-    sleep(90);
-    return 0;
+    puts("load http://localhost:9000/potaspot.xex");
+    sleep(30);
+    exit(0);
 }
